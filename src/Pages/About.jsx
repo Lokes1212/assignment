feddf19b9e047b7381ca2fbd877522592d7a2db0
techysiddhant <sidjain8000@gmail.com>
2023-07-25 19:32:53 +0530

    added about us component

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,12 @@
+import Navbar from "../components/navbar/Navbar";
+import Hero from "../components/HeroSection/Hero";
 const About = () => {
-	return <div>About</div>;
+	return (
+		<section>
+			<Navbar />
+			<Hero />
+		</section>
+	);
 };
 
 export default About;
